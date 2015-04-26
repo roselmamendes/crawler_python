5b9172f0268e9fefac4211667a0938e319478e73
Roselma Mendes <roselma.mendes@gmail.com>
2015-04-26 15:14:24 -0300

    Remove unnecessary line using SoupStrainer.

--- crawler.py
+++ crawler.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup, SoupStrainer
 def search(word):
     response = requests.get('http://www.google.com/search', params={'q': word})
 
-    soup_strainer = SoupStrainer('li.g')
     soup = BeautifulSoup(response.text)
     li_tags = soup.select('li.g')
     
